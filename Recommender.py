f6f332f5a805d81b214c7d90248226deff272b52
T1nyTim <samj.itunes@gmail.com>
2017-05-17 22:30:16 +1200

    cleaned it up

--- Recommender.py
+++ Recommender.py
@@ -11,50 +11,38 @@ def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
         f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
         print(*map(f, objects), sep=sep, end=end, file=file)
 
-p = requests.get("https://myanimelist.net/animelist/T1nyTim?status=2&tag")
-t = html.fromstring(p.content)
-aID = t.xpath('//body/div/div/@id')
-aT = t.xpath('//body/div/table/tr/td/a/span/text()')
+page = requests.get("https://myanimelist.net/animelist/T1nyTim?status=2&tag")
+tree = html.fromstring(page.content)
+animeUrl = tree.xpath('//body/div/table/tr/td/a/@href')
+animeTitle = tree.xpath('//body/div/table/tr/td/a/span/text()')
+animeUrl = [x for x in animeUrl if x[:6] == "/anime"]
 
-for i in range(0, len(aID)):
-    if aID[i - 1][:4] == "more":
-        aID[i - 1] = aID[i - 1][4:]
+for i in animeUrl:
+    page = requests.get("https://myanimelist.net" + i + "/userrecs")
+    tree = html.fromstring(page.content)
+    recs = tree.xpath('//div[@style="margin-bottom: 2px;"]/a/strong/text()')
+    num = tree.xpath('//div/a[@href="javascript:void(0);"]/strong/text()')
+
+    for j in range(0, len(num)):
+        num[j] = int(num[j]) + 1
+
+    blanks = len(recs) - len(num)
+
+    if blanks > 0:
+        for j in range(0, blanks):
+            num.append(1)
+
+    zippedList = list(zip(recs, num))
+    zippedList = [x for x in zippedList if x[0] not in animeTitle]
+
+    if i == animeUrl[0]:
+        recDict = dict(zippedList)
     else:
-        del aID[i - 1]
+        tmpDict = dict(zippedList)
+        input = [dict(x) for x in (recDict, tmpDict)]
+        recDict = sum((Counter(y) for y in input), Counter())
 
-a = []
+recDict = dict(recDict.most_common())
 
-for i in range(0, len(aID)):
-    p = requests.get("https://myanimelist.net/anime/" + str(aID[i]))
-    t = html.fromstring(p.content)
-    a = a + (t.xpath('//head/link[1]/@href'))
-
-for i in range(0, len(a)):
-    p = requests.get(a[i] + "/userrecs")
-    t = html.fromstring(p.content)
-    r = t.xpath('//div[@style="margin-bottom: 2px;"]/a/strong/text()')
-    n = t.xpath('//div/a[@href="javascript:void(0);"]/strong/text()')
-
-    for j in range(0, len(n)):
-        n[j] = int(n[j]) + 1
-
-    b = len(r) - len(n)
-
-    if b > 0:
-        for j in range(0, b):
-            n.append(1)
-
-    z = list(zip(r, n))
-    z = [x for x in z if x[0] not in aT]
-
-    if i == 0:
-        d = dict(z)
-    else:
-        tmp = dict(z)
-        input = [dict(x) for x in (d, tmp)]
-        d = sum((Counter(y) for y in input), Counter())
-
-d = dict(d.most_common())
-
-for i in d:
-    uprint(i, "\nRecommended", d[i], "times\n")
+for i in recDict:
+    uprint(i, "\nRecommended", recDict[i], "times\n")
