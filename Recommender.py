86da0f1098fdb06e77244ee6c3f2a7d6c21b3784
T1nyTim <samj.itunes@gmail.com>
2017-05-17 22:43:38 +1200

    cleaned more

--- Recommender.py
+++ Recommender.py
@@ -12,37 +12,36 @@ def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
         print(*map(f, objects), sep=sep, end=end, file=file)
 
 page = requests.get("https://myanimelist.net/animelist/T1nyTim?status=2&tag")
-tree = html.fromstring(page.content)
-animeUrl = tree.xpath('//body/div/table/tr/td/a/@href')
-animeTitle = tree.xpath('//body/div/table/tr/td/a/span/text()')
-animeUrl = [x for x in animeUrl if x[:6] == "/anime"]
 
-for i in animeUrl:
-    page = requests.get("https://myanimelist.net" + i + "/userrecs")
+if page.status_code == 200:
     tree = html.fromstring(page.content)
-    recs = tree.xpath('//div[@style="margin-bottom: 2px;"]/a/strong/text()')
-    num = tree.xpath('//div/a[@href="javascript:void(0);"]/strong/text()')
+    animeUrl = tree.xpath('//body/div/table/tr/td/a/@href')
+    animeTitle = tree.xpath('//body/div/table/tr/td/a/span/text()')
+    animeUrl = [x for x in animeUrl if x[:6] == "/anime"]
 
-    for j in range(0, len(num)):
-        num[j] = int(num[j]) + 1
+    for i in animeUrl:
+        page = requests.get("https://myanimelist.net" + i + "/userrecs")
 
-    blanks = len(recs) - len(num)
+        if page.status_code == 200:
+            tree = html.fromstring(page.content)
+            recs = tree.xpath('//div[@style="margin-bottom: 2px;"]/a/strong/text()')
+            num = tree.xpath('//div/a[@href="javascript:void(0);"]/strong/text()')
+            num = [int(x) + 1 for x in num]
 
-    if blanks > 0:
-        for j in range(0, blanks):
-            num.append(1)
+            for j in range(len(num), len(recs)):
+                num.append(1)
 
-    zippedList = list(zip(recs, num))
-    zippedList = [x for x in zippedList if x[0] not in animeTitle]
+            zippedList = list(zip(recs, num))
+            zippedList = [x for x in zippedList if x[0] not in animeTitle]
 
-    if i == animeUrl[0]:
-        recDict = dict(zippedList)
-    else:
-        tmpDict = dict(zippedList)
-        input = [dict(x) for x in (recDict, tmpDict)]
-        recDict = sum((Counter(y) for y in input), Counter())
+            if i == animeUrl[0]:
+                recDict = dict(zippedList)
+            else:
+                tmpDict = dict(zippedList)
+                input = [dict(x) for x in (recDict, tmpDict)]
+                recDict = sum((Counter(y) for y in input), Counter())
 
-recDict = dict(recDict.most_common())
+    recDict = dict(recDict.most_common())
 
-for i in recDict:
-    uprint(i, "\nRecommended", recDict[i], "times\n")
+    for i in recDict:
+        uprint(i, "\nRecommended", recDict[i], "times\n")
